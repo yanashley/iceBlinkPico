128c2af24615204e7110dfed41fd94de874b109f
bminch <bradley.minch@olin.edu>
2025-02-08 15:57:06 -0500

    Fixed comment in pwm.sv in fade example.

--- examples/fade/pwm.sv
+++ examples/fade/pwm.sv
@@ -11,7 +11,7 @@ module pwm #(
     // Declare PWM generator counter variable
     logic [$clog2(PWM_INTERVAL) - 1:0] pwm_count = 0;
 
-    // Implement counter for timing state transitions
+    // Implement counter for timing transition in PWM output signal
     always_ff @(posedge clk) begin
         if (pwm_count == PWM_INTERVAL - 1) begin
             pwm_count <= 0;
