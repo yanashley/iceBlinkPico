d67b7aa40b2916913c891ea97a6a99e599fdd103
bminch <bradley.minch@olin.edu>
2025-02-10 13:57:34 -0500

    Changed fade_tb.sv to instance top module.

--- examples/fade/fade_tb.sv
+++ examples/fade/fade_tb.sv
@@ -1,35 +1,24 @@
 `timescale 10ns/10ns
-`include "fade.sv"
-`include "pwm.sv"
+`include "top.sv"
 
 module fade_tb;
 
     parameter PWM_INTERVAL = 1200;
 
     logic clk = 0;
-    logic [$clog2(PWM_INTERVAL) - 1:0] pwm_value;
-    logic pwm_out;
+    logic LED;
 
-    fade #(
+    top # (
         .PWM_INTERVAL   (PWM_INTERVAL)
     ) u0 (
         .clk            (clk), 
-        .pwm_value      (pwm_value)
+        .LED            (LED)
     );
-
-    pwm #(
-        .PWM_INTERVAL   (PWM_INTERVAL)
-    ) u1 (
-        .clk            (clk), 
-        .pwm_value      (pwm_value), 
-        .pwm_out        (pwm_out)
-    );
-
 
     initial begin
         $dumpfile("fade.vcd");
         $dumpvars(0, fade_tb);
-        #100000000
+        #60000000
         $finish;
     end
 
